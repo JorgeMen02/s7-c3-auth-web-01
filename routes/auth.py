b7dd091f7b6db600aa5c90836c7caf0aa9b6df39
balbino aylagas <balbino_a@hotmail.com>
2022-03-07 21:32:08 -0600

    added templates

--- routes/auth.py
+++ routes/auth.py
@@ -1,8 +1,23 @@
-from flask import Blueprint
+from flask import Blueprint, render_template
 
 auth = Blueprint("auth", __name__)
 
 
 @auth.route("/")
 def home():
-    return "auth home page"
+    return render_template("home.html")
+
+
+@auth.route("/login")
+def login():
+    return render_template("login.html")
+
+
+@auth.route("/register")
+def register():
+    return render_template("register.html")
+
+
+@auth.route("/dashboard")
+def dashboard():
+    return render_template("dashboard.html")
