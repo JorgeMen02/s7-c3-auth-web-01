9be01bcb993a19ead82d50621237a997df8c328e
balbino aylagas <balbino_a@hotmail.com>
2022-03-07 21:40:26 -0600

    added mysqlclient and sqlalchemy config

--- app.py
+++ app.py
@@ -1,6 +1,13 @@
 from flask import Flask
+from flask_sqlalchemy import SQLAlchemy
 from routes.auth import auth
 
 app = Flask(__name__)
+
+# configurar flask alchemy
+app.config.from_object("config.BaseConfig")
+print(app.config)
+
+SQLAlchemy(app)
 
 app.register_blueprint(auth)
